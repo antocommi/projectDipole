bd3a5d90878b606e177d8ef5241c6cf65bec4936
antocommi <commisso.antonio@outlook.it>
2019-11-09 16:25:13 +0100

    ok

--- src/progDipole/Dipole.java
+++ src/progDipole/Dipole.java
@@ -1,13 +1,15 @@
 package progDipole;
+
 class DipoleLogica{
     
     String prova(){
         return "";
     }
+
     String eseguiMossa(String mossa){
         return "null";
     }
-
+    
     public static void main(String[] args){
         System.out.println("ciao");
     }
