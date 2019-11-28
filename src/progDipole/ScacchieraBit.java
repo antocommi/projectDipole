ba41cabfee5482b167e24e4002af7132730a1742
antocommi <commisso.antonio@outlook.it>
2019-11-28 12:00:07 +0100

    ok

--- src/progDipole/ScacchieraBit.java
+++ src/progDipole/ScacchieraBit.java
@@ -1,20 +1,51 @@
 package progDipole;
 
-import util.BitMap;
+import util.ByteMap;;
 
 public class ScacchieraBit {
 	// Implementazione della scacchiera tramite bitboard. 
 	// La scacchiera è 8x8 => 64 celle
 	// Due bitboard da 64 bit una per i neri e l'altra per i bianchi
 //	private int scacchiere;
-	private BitMap whiteBoard, blackBoard ;
+	
+	private final int DIMENSION = 8;
+	private ByteMap whiteMap, blackMap;
 	private boolean turnoGiocatore;
+	private int scacchieraBianchi, scacchieraNeri;
+	
+	// isBlack(i,j) -> (((1 << i*8+j) & scacchieraBianchi) >> i*8+j) == 1
+	
+	
+	private int modifyBit(int numero, int posizione, int valBinario) {
+		int mask = 1 << posizione;
+		return (numero & ~mask) | ((valBinario << posizione) & mask);
+	}
+	
+	private int getPositionOnBoard(int i, int j) {
+		return i*8+j;
+	}
+	
+	private void addOnBoard(int i, int j, int color, int qty) {
+		if(color==1) {
+			scacchieraBianchi =modifyBit(1, getPositionOnBoard(i,j), scacchieraBianchi);
+		}
+		else if(color==0) {
+			scacchieraNeri = modifyBit(1, getPositionOnBoard(i,j), scacchieraNeri);
+		}
+	}
 	
 	public ScacchieraBit() {
 		turnoGiocatore = true;
 //		scacchiere = 0b01000000000000000000000000000100;
-		pedine = new BitMap(16);
+		scacchieraBianchi = 0;
+		scacchieraNeri = 0;
+		scacchieraBianchi = modifyBit(1, getPositionOnBoard(0, 3), scacchieraBianchi);
+		scacchieraNeri = modifyBit(1, getPositionOnBoard(7, 4), scacchieraNeri);
+		whiteMap = new ByteMap(DIMENSION*DIMENSION);
+		blackMap = new ByteMap(DIMENSION*DIMENSION); 
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Done!");
--- src/util/ByteMap.java
+++ src/util/ByteMap.java
@@ -5,24 +5,39 @@ public class ByteMap {
 	private int mask = 0b00000000000000000000000000000111;
 	private int nBitIndex = 4;
 //	~ negazione
-	
+	// NUMERO DI VALORE PER INTERO: 8 se |valore|=4 bit
 	public ByteMap(int capacity) {
 		bitMap = new int[capacity];
 	}
 	
 	public void setValue(int value, int index) {
+//		index è un valore compreso tra 0 e capacity*8
 		int k,l;
 		k = index % 8;
 		l = index / 8;
-		int n = bitMap[k];
-		// (n & ~mask ) | ( (b << p) & mask )
-		value = (n & ~mask) & ( mask << 8 - l );  
-		
+		int n = bitMap[l], m  = mask << k*3 ;
+		bitMap[l] = ( n & ~ m ) | ( ( value << k*3 ) & m );  
+	}
+	
+	public int[] getbm() {
+		return bitMap;
+	}
+	
+	public int getIndex(int x, int y) {
+		return x*8+y;
+	}
+	
+	public int getValue(int index) {
+		// index \in [0, 8* bitMap.len ]
+		int i = index % 8, j = index / 8, n;
+		n = bitMap[j];
+		return (n & (mask << i*3)) ;//>> (8 - i)*3;
 	}
 	
 	public static void main(String[] args) {
-		int a=0b1,b=0b1;
-		a = a << 1;
-		System.out.println(a);
+		ByteMap bm = new ByteMap(4);
+		bm.setValue(2,1);
+		
+		System.out.println(bm.getValue(1)+" "+bm.getbm()[0]);
 	}
 }
