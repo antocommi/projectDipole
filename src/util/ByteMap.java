0244a80a6e42e6237304cb3e4781422a92b2b2b4
antocommi <commisso.antonio@outlook.it>
2019-12-17 18:30:03 +0100

    ok

--- src/util/ByteMap.java
+++ src/util/ByteMap.java
@@ -2,10 +2,10 @@ package util;
 
 public class ByteMap {
 	private int[] bitMap;
-	private int mask = 0b00000000000000000000000000000111;
+	private int mask = 0b1111;
 	private int nBitIndex = 4;
 
-//	~ negazione
+	// ~ negazione
 	// NUMERO DI VALORE PER INTERO: 8 se |valore|=4 bit
 	public ByteMap(int capacity) {
 		bitMap = new int[capacity];
@@ -16,8 +16,9 @@ public class ByteMap {
 		int k, l;
 		k = index % 8;
 		l = index / 8;
-		int n = bitMap[l], m = mask << k * 4;
-		bitMap[l] = (n & ~m) | ((value << k * 4) & m);
+		int shiftWidth = (7-k) * 4;
+		int n = bitMap[l], m = mask << shiftWidth;
+		bitMap[l] = (n & ~m) | ( (value << shiftWidth) & m);
 	}
 
 	public int[] getbm() {
@@ -28,19 +29,37 @@ public class ByteMap {
 		return x * 8 + y;
 	}
 
-	public int getValue(int index) {
+	public String getValue(int index) {
 		// index \in [0, 8* bitMap.len ]
 		int i = index % 8, j = index / 8, n;
 		n = bitMap[j];
-		return (n & (mask << i * 4));// >> (8 - i)*3;
+		return Integer.toBinaryString( 
+				(n & (mask << (7-i) * 4)) >> (7-i)*4
+						);
 	}
 
-	public int getNumeroPedine(int i, int j) {
-		return getValue(i*8+j);
+//	public int getNumeroPedine(int i, int j) {
+//		return getValue(i*8+j);
+//	}
+	
+	public void printValues() {
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<8;j++) {
+				System.out.print(" "+this.getValue(i+j));
+			}
+			System.out.println("");
+		}
+		
+		for(int k=0;k<8;k++) {
+			System.out.println(Integer.toBinaryString(bitMap[k]));
+		}
 	}
 	
 	public static void main(String[] args) {
-		
+		ByteMap bm = new ByteMap(8);
+		bm.setValue(13, 0);
+		bm.printValues();
+//		System.out.println(bm.getValue(2));
 	}
 
 }
