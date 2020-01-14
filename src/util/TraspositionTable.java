5da195f8ca1f2f82a9ab225bacdce23b91456e94
antocommi <commisso.antonio@outlook.it>
2020-01-14 10:14:39 +0100

    Aggiunto metodo contains

--- src/util/TraspositionTable.java
+++ src/util/TraspositionTable.java
@@ -2,24 +2,29 @@ package util;
 
 import Dipole.TTElement;
 
-public class TraspositionTable{
-	
+public class TraspositionTable {
+
 	private TTElement[] table;
 	private int size;
-	
-	//TODO: Valutare cosa inserire in TTElement, ad esempio la chiave. 
-	
-	public TraspositionTable(int size){
+
+	// TODO: Valutare cosa inserire in TTElement, ad esempio la chiave.
+
+	public TraspositionTable(int size) {
 		super();
 		this.size = size;
 		table = new TTElement[size];
+	}
+
+	// TODO: Sistemare il metodo equals di TTElement altrimenti NON FUNZIONA
+	public boolean contains(TTElement e) {
+		return table[(int) (e.getKey() % size)].equals(e);
 	}
 
 	public void put(long key, TTElement value) {
 		// TODO: Replacement scheme - Ora politica LIFO
 		table[(int) (key % size)] = value;
 	}
-	
+
 	public TTElement get(long key) {
 		return table[(int) (key % size)];
 	}
